33831f2982419cd706b0c91aa356755c183c6fcc
itzelg16 <71837145+itzelg16@users.noreply.github.com>
2023-09-25 17:52:09 -0500

    Create createDatabase.js

---
+++ back-end/Database Connection/createDatabase.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+	host:'localhost',
+	port:3306,
+	user:'root',
+	password:''
+});
+
+
+connection.connect(function(error){
+	if(error)
+	{
+		console.log('Cannot connect to database.');
+	}
+	else{
+		console.log('Connection to database successful.');
+	}
+});
